3ffe394d2bce5c3da33d19c4fa63b69022ac3703
Parker Johnson <enragedboar@gmail.com>
2015-07-30 15:00:12 -0700

    extracted all meaningful data from front of cards, cleaned code

--- apartment_webscraper.py
+++ apartment_webscraper.py
@@ -16,11 +16,8 @@ def scrape():
             "accept-encoding": "gzip,deflate,sdch",
             "accept-language": "en-US,en;q=0.8"
         }
-
-    # request the url, push the headers, store the response
     response = requests.get(url, headers=headers)
 
-    # check for gucci status code
     if response.status_code != 200:
         print("request denied")
         return
@@ -28,45 +25,47 @@ def scrape():
         print("scraping " + url)
 
     soup = BeautifulSoup(response.text, "lxml")
-    # assigns ResultSet of cards to "cards"
-    # these are tags
-    cards = soup.find_all("div", "available-apartment-card")
 
-    # get a ton of stuff done
-    results = get_card_data(cards)
+    cards_soup = soup.find_all("div", "available-apartment-card")
+
+    # shit goes down right here
+    results = get_card_data(cards_soup)
 
     pprint.pprint(results)
 
 
 def parse_name(card):
-    return card.find("div", "panel-pane pane-entity-field pane-node-field-title-display inverted pos-left pos-bottom").string.strip()
+    # not returning data to unicode with str() would pass expensive Bs4 objects
+    return str(card.find("div", "panel-pane pane-entity-field pane-node-field-title-display inverted pos-left pos-bottom").string.strip())
 
 
 def parse_price(card):
-    # These originally parse out with a span tag in front, contents[2] fixes
-    # removed newline and $, strips out extra spacing
-    price = card.find("div", "price").contents[2].replace("\n", "").replace("$", "").strip()
-    return price
+    # Replaces newline and $
+    return str(card.find("div", "price").contents[2].replace("\n", "").replace("$", "").strip())
 
 
 def parse_cheapest_apartment_number(card):
-    return card.find("div", "unit-name").contents[0]
+    return str(card.find("div", "unit-name").contents[0])
 
 
-def get_card_data(cards):
-        # create a list
+def parse_move_in(card):
+    return str(card.find("div", "move-in").contents[1]).strip()
+
+
+def parse_lease_lenth(card):
+    return str(card.find("div", "lease-contract").contents[1])
+
+
+def get_card_data(cards_soup):
         data_list = list()
-        # for each card in our list of cards, parse important info
-        for card in cards:
-            name = parse_name(card)
-            lowest_price = parse_price(card)
-            cheapest_apartment = parse_cheapest_apartment_number(card)
+        # for each card in our list of cards_soup, parse important info
+        for card in cards_soup:
+            data = {"Lowest Price": parse_price(card),
+                    "Style": parse_name(card),
+                    "Apartment Number": parse_cheapest_apartment_number(card),
+                    "Lease Length": parse_lease_lenth(card),
+                    "Move In Day": parse_move_in(card)}
 
-            # put data in a dictionary, str removes what looks like bs4
-            data = {"Lowest Price": str(lowest_price),
-                    "Name": str(name),
-                    "Cheapest Apartment": str(cheapest_apartment)}
-            # add card's dictionary to a list of cards
             data_list.append(data)
 
         return data_list
